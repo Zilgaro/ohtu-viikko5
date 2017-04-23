855d7e1722191d6818b1b510612d444d08eeb039
Zilgaro <verner_saaristo@msn.com>
2017-04-23 17:35:22 +0300

    Tehtävä 2

--- MatkakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
+++ MatkakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
@@ -40,4 +40,18 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleVoiLadataPositiivisenArvon() {
+        kassa.lataa(kortti, 10);
+        
+        verify(kortti,times(1)).lataa(10);
+    }
+    
+    @Test
+    public void kortilleEiVoiLadataNegatiivistaArvoa(){
+        kassa.lataa(kortti, -10);
+        
+        verify(kortti,times(0)).lataa(-10);
+    }
 }
